8446accb161f54ced1c81771c3cd143a854593be
Harald Haraldsson <530725+haraldsson@users.noreply.github.com>
2025-03-15 22:48:30 -0400

    Update dynamixel_controller_node.py

--- xrc_motor/xrc_motor/dynamixel_controller_node.py
+++ xrc_motor/xrc_motor/dynamixel_controller_node.py
@@ -1,20 +1,7 @@
 #!/usr/bin/env python3
 
-# Copyright 2021 ROBOTIS CO., LTD.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Modified for flexible parameter-based configuration with AX-12A defaults
+# Based on read_write_node.py example from dynamixel_sdk_examples by Robotis.
+# Modified for flexible parameter-based configuration with AX-12A defaults, and different protocol settings.
 
 from dynamixel_sdk import COMM_SUCCESS
 from dynamixel_sdk import PacketHandler
